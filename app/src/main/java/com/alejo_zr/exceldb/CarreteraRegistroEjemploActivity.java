eea5dee2c5f98d2a11f05c738006feebb4cb521a
amzr36 <amzr36@gmail.com>
2017-10-18 20:05:29 -0500

    Initial commit

---
+++ app/src/main/java/com/alejo_zr/exceldb/CarreteraRegistroEjemploActivity.java
@@ -0,0 +1,13 @@
+package com.alejo_zr.exceldb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarreteraRegistroEjemploActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carretera_registro_ejemplo);
+    }
+}
